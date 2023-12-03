8b9bfe1f91841d5c202aab3f09a56168859a5d25
Rita Epanc <ritkabanditka59@gmail.com>
2023-12-03 14:15:12 +0600

    add StringProcessor commit

--- src/main/java/org/example/FinanceReportProcessor.java
+++ src/main/java/org/example/FinanceReportProcessor.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class FinanceReportProcessor {
     public void symbolOfPayments(char symbol, FinanceReport report){
+        System.out.println("chill out bro");
 
     }
 }
